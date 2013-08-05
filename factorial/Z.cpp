974b5c27c90530a2f671cb20a826a870010b573f
z01tan <arhi-t@yandex.ru>
2013-08-05 23:25:09 +0400

    true Z, but exid time limit

--- factorial/Z.cpp
+++ factorial/Z.cpp
@@ -1,75 +1,17 @@
 #include <iostream>
-#include <sstream>
-#include <cmath>
 
-template <typename T>
-unsigned long long numberDigits(T num)
+int Z(int N)
 {
-  unsigned long long ndigits=0;
-  while(num)
+  int acc = 0;
+  for(int i =0;i<=N;i+=5)
     {
-      num /= 10;
-      ++ndigits;
-    }
-  return ndigits;
-}
-
-int stupidZerosCount(unsigned long long a)
-{
-  std::stringstream ss;
-  ss<<a;
-  std::string str = ss.str();
-  int count=0;
-  for(int i = str.size()-1;i>=0;--i){
-    if(str[i]=='0')
-      ++count;
-    else
-      return count;
-  }
-  return count;
-}
-
-unsigned long long factorial(unsigned long long N)
-{
-  if(N==1)
-    return 1;
-  if(N<0)
-    return -1;
-  unsigned long long acc=1;
-  for(unsigned long long i=1;i<=N;++i)
-    {
-      //      std::cout<<i<<" "<<acc<<std::endl;
-      acc*=i;
-    }
-
-  return acc;
-}
-
-unsigned long long Z(unsigned long long N)
-{
-  unsigned long long count = numberDigits<unsigned long long>(N);
-  std::cout<<count<<std::endl;
-  unsigned long long acc = 0;
-  unsigned long long decs;
-  for(int i =1;i<=count;++i)
-    {
-      std::cout<<" i = "<<i<<std::endl;
-      decs = pow(10,i);
-      if(i==1)
+      int local = i;
+      while((local%5)==0 and local!=0)
 	{
-	  acc+=N/decs;
-	  if(N%10>=5)
-	    ++acc;
+	  ++acc;
+	  local/=5;
 	}
-      if(i==2)
-	{
-	  acc+=N/25;
-	}
-      if(i==3)
-	{
-	  acc+=N/125;
-	}
-      acc += (N/decs);
+
     }
   
   return acc;
@@ -77,12 +19,12 @@ unsigned long long Z(unsigned long long N)
 
 int main()
 {
-  unsigned long long N;
-  std::cout<<"N :";
-  std::cin>>N;
-  //dev  for(int i=2;i<N;++i)
-  std::cout<<"Z("<<N<<") = "<<Z(N)<<std::endl;
-  // std::cout<<"fctrl = "<<factorial(N)<<" Z(N) = "<<stupidZerosCount(factorial(N))<<std::endl;
-  //  std::cout<<"Z(60) = "<<Z(60)<<"; fctrl 60 = "<<factorial(60)<<std::endl;
+  int count,N;
+  std::cin>>count;
+  for(int i=0;i<count;++i){
+    std::cin>>N;
+    std::cout<<Z(N)<<std::endl;
+  }
+
   return 0;
 }
