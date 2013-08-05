e8013fa1cde2d4ad894f52ae6613613eaee327f9
z01tan_work <arhi-t@yandex.ru>
2013-08-05 11:24:59 +0400

    add stupid function conunt zeros by string

--- factorial/Z.cpp
+++ factorial/Z.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <sstream>
 #include <cmath>
 
 template <typename T>
-long long numberDigits(T num)
+unsigned long long numberDigits(T num)
 {
-  long long ndigits=0;
+  unsigned long long ndigits=0;
   while(num)
     {
       num /= 10;
@@ -13,26 +14,43 @@ long long numberDigits(T num)
   return ndigits;
 }
 
-long long factorial(long long N)
+int stupidZerosCount(unsigned long long a)
+{
+  std::stringstream ss;
+  ss<<a;
+  std::string str = ss.str();
+  int count=0;
+  for(int i = str.size()-1;i>=0;--i){
+    if(str[i]=='0')
+      ++count;
+    else
+      return count;
+  }
+  return count;
+}
+
+unsigned long long factorial(unsigned long long N)
 {
   if(N==1)
     return 1;
   if(N<0)
     return -1;
-  long long acc=1;
-  for(long long i=1;i<=N;++i)
+  unsigned long long acc=1;
+  for(unsigned long long i=1;i<=N;++i)
     {
+      std::cout<<i<<" "<<acc<<std::endl;
       acc*=i;
     }
+
   return acc;
 }
 
-long long Z(long long N)
+unsigned long long Z(unsigned long long N)
 {
-  long long count = numberDigits<long long>(N);
-  long long acc = 0;
-  long long decs;
-  for(long long i =1;i<=count;++i)
+  unsigned long long count = numberDigits<unsigned long long>(N);
+  unsigned long long acc = 0;
+  unsigned long long decs;
+  for(unsigned long long i =1;i<=count;++i)
     {
       decs = pow(10,i);
       if(i==1)
@@ -49,11 +67,12 @@ long long Z(long long N)
 
 int main()
 {
-  long long N;
+  unsigned long long N;
   std::cout<<"N :";
   std::cin>>N;
-  for(int i=2;i<N;++i)
-    std::cout<<"Z("<<i<<") = "<<Z(i)<<" fctrl = "<<factorial(i)<<std::endl;
+  /*  for(int i=2;i<N;++i)
+      std::cout<<"Z("<<i<<") = "<<Z(i)<<" fctrl = "<<factorial(i)<<std::endl;*/
+  std::cout<<"fctrl = "<<factorial(N)<<" Z(N) = "<<stupidZerosCount(factorial(N))<<std::endl;
   //  std::cout<<"Z(60) = "<<Z(60)<<"; fctrl 60 = "<<factorial(60)<<std::endl;
   return 0;
 }
