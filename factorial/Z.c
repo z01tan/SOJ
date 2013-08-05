abb7bde27f5958c750b7785a8f1d9c8b5b060d23
z01tan <arhi-t@yandex.ru>
2013-08-06 00:57:54 +0400

    add pure c version

---
+++ factorial/Z.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int Z(int N)
+{
+  int acc = 0;
+  int i;
+  for(i =0;i<=N;i+=5)
+    {
+      int local = i;
+      while((local%5)==0 || local!=0)
+	{
+	  ++acc;
+	  local/=5;
+	}
+
+    }
+  
+  return acc;
+}
+
+int main()
+{
+  int count,N,i;
+  printf("N:");
+  scanf("%d",count);
+  for(i=0;i<count;++i){
+    scanf("%d",N);
+    printf("%d\n",Z(N));
+  }
+
+  return 0;
+}
